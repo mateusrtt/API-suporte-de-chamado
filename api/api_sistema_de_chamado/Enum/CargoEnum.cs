c90b2dc87c3a1c847a2a34cdd82d75c96a2a5e87
mateusrtt <mateuseduardo779@gmail.com>
2025-07-05 17:43:10 -0300

    Criação do model usuario e do enum cargo

---
+++ api/api_sistema_de_chamado/Enum/CargoEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace api_sistema_de_chamado.Enum
+{
+    public enum CargoEnum
+    {
+        Usuario = 1,
+        Administrador = 2,
+    }
+}
---
+++ api/api_sistema_de_chamado/Models/UsuariosModel.cs
@@ -0,0 +1,16 @@
+﻿using api_sistema_de_chamado.Enum;
+
+namespace api_sistema_de_chamado.Models
+{
+    public class UsuariosModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public CargoEnum Cargo { get; set; } 
+        public byte[] SenhaHash { get; set; }
+        public byte[] SenhaSalt { get; set; }
+        public DateTime TokenDataCriacao { get; set; } = DateTime.Now;
+
+    }
+}
