fc8eb30b85bab883ac49ad95b51f826d66bb3a2d
mateusrtt <mateuseduardo779@gmail.com>
2025-07-05 18:33:48 -0300

    Criação do AuthController

---
+++ api/api_sistema_de_chamado/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_sistema_de_chamado.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
