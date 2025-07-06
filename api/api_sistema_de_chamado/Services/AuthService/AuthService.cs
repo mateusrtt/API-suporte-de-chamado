c343eb2e8989e775e2afeec73fa1bc225963d623
mateusrtt <mateuseduardo779@gmail.com>
2025-07-06 15:46:46 -0300

    Modelo response genérico de resposta para API criado

--- api/api_sistema_de_chamado/Services/AuthService/AuthService.cs
+++ api/api_sistema_de_chamado/Services/AuthService/AuthService.cs
@@ -1,11 +1,12 @@
 ﻿using api_sistema_de_chamado.Dtos;
+using api_sistema_de_chamado.Models;
 
 namespace api_sistema_de_chamado.Services.AuthService
 {
     public class AuthService : IAuthInterface
     {
 
-        public async Task<UsuarioCriacaoDto> Registrar(UsuarioCriacaoDto usuarioRegister)
+        public async Task<Response<UsuarioCriacaoDto>> Registrar(UsuarioCriacaoDto usuarioRegister)
         {
             
         }
