93a788f77f72b6765b76175e1d15470121b962c9
mateusrtt <mateuseduardo779@gmail.com>
2025-07-07 17:28:57 -0300

    Configurei a autenticação JWT e integração com Swagger para uso de token nas rotas protegidas

--- api/api_sistema_de_chamado/Program.cs
+++ api/api_sistema_de_chamado/Program.cs
@@ -2,8 +2,13 @@ using api_sistema_de_chamado.Data;
 using api_sistema_de_chamado.Repositories.Usuario;
 using api_sistema_de_chamado.Services.AuthService;
 using api_sistema_de_chamado.Services.SenhaService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +30,33 @@ builder.Services.AddDbContext<AppDbContext>(Options =>
     Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+// Configura Swagger e validação JWT (Bearer) para a API
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("ouath2", new OpenApiSecurityScheme
+    {
+        Description = "Standar Authorization header using the Bearer scheme (\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+        ValidateAudience = false,
+        ValidateIssuer = false,
+    };
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -35,6 +67,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
