fb85eede4236da8534f7af43c2afd6fab42faa4d
mateusrtt <mateuseduardo779@gmail.com>
2025-07-17 17:16:33 -0300

    test: adicionei teste para falha na verificação de senha e teste de criação de token JWT

--- api/api_sistema_de_chamado_tests/SenhaServiceTests.cs
+++ api/api_sistema_de_chamado_tests/SenhaServiceTests.cs
@@ -1,8 +1,11 @@
-﻿using api_sistema_de_chamado.Services.SenhaService;
+﻿using api_sistema_de_chamado.Enum;
+using api_sistema_de_chamado.Models;
+using api_sistema_de_chamado.Services.SenhaService;
 using Microsoft.Extensions.Configuration;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +20,7 @@ namespace api_sistema_de_chamado_tests
         public SenhaServiceTests()
         {
             _configurationMock = new Mock<IConfiguration>();
-            _configurationMock.Setup(x => x.GetSection("AppSettings:Token").Value).Returns("supersecretkeyforsigningtoken");
+            _configurationMock.Setup(x => x.GetSection("AppSettings:Token").Value).Returns("0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef");
 
             _senhaService = new SenhaService(_configurationMock.Object);
         }
@@ -50,6 +53,44 @@ namespace api_sistema_de_chamado_tests
 
             // Assert
             Assert.True(resultado);
+        }
+
+        [Fact]
+        public void Verifica_SenhaHash_Deve_Retornar_False_Para_SenhaErrada()
+        {
+            // Arrange
+            string senhaCorreta = "senha123";
+            string senhaErrada = "senhaErrada";
+            _senhaService.CriarSenhaHash(senhaCorreta, out byte[] hash, out byte[] salt);
+
+            // Act
+            bool resultado = _senhaService.VerificaSenhaHash(senhaErrada, hash, salt);
+
+            // Assert
+            Assert.False(resultado);
+        }
+
+        [Fact]
+        public void CriarToken_QuandoUsuarioValido_DeveGerarJwtTokenValido()
+        {
+            // Arrange
+            var usuario = new UsuariosModel
+            {
+                Nome = "Mateus",
+                Email = "mateus@email.com",
+                Cargo = CargoEnum.Usuario
+            };
+
+            // Act
+            var tokenGerado = _senhaService.CriarToken(usuario);
+
+            // Assert
+            Assert.False(string.IsNullOrWhiteSpace(tokenGerado)); // Garante que não é nulo ou vazio
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var podeLerToken = tokenHandler.CanReadToken(tokenGerado);
+
+            Assert.True(podeLerToken); // Confirma que é um JWT válido
         }
 
     }
