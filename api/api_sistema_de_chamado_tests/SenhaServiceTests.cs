9557bad22d5e9bc79b5daa3912a97b7ac1c95f67
mateusrtt <mateuseduardo779@gmail.com>
2025-07-17 16:13:03 -0300

    test: adicionei teste unitário para método CriarSenhaHash do SenhaService

---
+++ api/api_sistema_de_chamado_tests/SenhaServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using api_sistema_de_chamado.Services.SenhaService;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_sistema_de_chamado_tests
+{
+    public class SenhaServiceTests
+    {
+        private readonly SenhaService _senhaService;
+        private readonly Mock<IConfiguration> _configurationMock;
+
+        public SenhaServiceTests()
+        {
+            _configurationMock = new Mock<IConfiguration>();
+            _configurationMock.Setup(x => x.GetSection("AppSettings:Token").Value).Returns("supersecretkeyforsigningtoken");
+
+            _senhaService = new SenhaService(_configurationMock.Object);
+        }
+
+        [Fact]
+        public void CriarSenha_Deve_Gerar_Hash_e_Salt()
+        {
+            // Arrange
+            string senha = "senha123";
+
+            // Act
+            _senhaService.CriarSenhaHash(senha, out byte[] hash, out byte[] salt);
+
+            // Assert
+            Assert.NotNull(hash);
+            Assert.NotNull(salt);
+            Assert.NotEmpty(hash);
+            Assert.NotEmpty(salt);
+        }
+
+    }
+}
